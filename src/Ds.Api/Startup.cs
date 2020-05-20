39fa411dee3eb6c0a758173666d4c0719734ec6d
David Pereira <davidpagani@hotmail.com>
2020-05-20 19:04:56 -0300

    Atualizações para Azure

--- src/Ds.Api/Startup.cs
+++ src/Ds.Api/Startup.cs
@@ -10,14 +10,29 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System;
 
 namespace Ds.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration configuration;
+        public Startup(IHostEnvironment hostEnvironment)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+
+            //Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
